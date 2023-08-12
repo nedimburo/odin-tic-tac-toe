d9868c4d33728fafa46342062b1690169660b7cd
nedimburo <nedimb9402@gmail.com>
2023-08-12 23:32:24 +0200

    Created player name registration, updated modules

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const gameBoard=(()=>{
+    let playerOne;
     let gameBoardArray=["X", "O", "X", "O", "X", "O", "X", "O", "X"];
     const getArray=()=>gameBoardArray;
+    const setPlayerOne=player=>playerOne=player;
+    const getPlayerOne=()=>playerOne;
     return{
         getArray,
+        setPlayerOne,
+        getPlayerOne,
     }
 })();
 
 const displayController=(()=>{
     const generateGrid=()=>{
         let display=document.getElementById("game-container");
+        display.innerHTML="";
         let mainDiv=document.createElement("div");
         mainDiv.setAttribute("id", "grid-container");
         for (let i=0; i<gameBoard.getArray().length; i++){
@@ -19,15 +25,35 @@ const displayController=(()=>{
         }
         display.appendChild(mainDiv);
     };
+    const getFormDetails=()=>{
+        let playersName=document.getElementById("username").value;
+        let player=Player(playersName, "X");
+        gameBoard.setPlayerOne(player);
+        console.log(gameBoard.getPlayerOne().getName()+" "+gameBoard.getPlayerOne().getMarker());
+        generateGrid();
+    };
+    const createForm=()=>{
+        let display=document.getElementById("game-container");
+        display.innerHTML=`<div id=form-container>
+                             <form id="user-input-form">
+                                <label for="username">Enter your name:</label><br>
+                                <input type="text" name="username" id="username">
+                             </form>
+                             <div id="form-button-container">
+                                <button onclick="displayController.getFormDetails()" class="submit-button">Submit</button>
+                             </div>
+                           </div>`;
+    };
     const startGame=()=>{
-        let startButton=document.getElementById("start-button");
+        let startButton=document.getElementById("start-button-container");
         startButton.style.display="none";
         let display=document.getElementById("game-container");
         display.style.display="block";
-        generateGrid()
+        createForm();
     };
     return{
         startGame,
+        getFormDetails,
     }
 })();
 
